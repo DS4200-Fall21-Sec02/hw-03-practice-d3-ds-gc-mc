4db49d085a5e3d1007061cf615bdbd55a29deaa9
danielleSiino23 <72044962+danielleSiino23@users.noreply.github.com>
2021-09-29 20:37:27 -0400

    Update main.js

--- hw-03-practice-d3-ds-gc-mc/js/main.js
+++ hw-03-practice-d3-ds-gc-mc/js/main.js
@@ -18,6 +18,53 @@ let svg1 = d3.select('#vis1')
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+  d3.csv("data/Priors_Clean.csv").then (function (data) {
+    const subgroups = data.columns.slice(1)
+
+    const years = data.map(d=> d.year)
+
+    console.log(years)
+
+    const x = d3.scaleBand().domain(years).range([0, width]).padding([.2])
+    svg1.append("g").attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x).tickSize(0));
+
+
+    const y = d3.scaleLinear()
+    .domain([0, 40])
+    .range([ height, 0 ]);
+  svg1.append("g")
+    .call(d3.axisLeft(y));
+
+
+    const xSubgroup = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05])
+
+
+    const color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#e41a1c','#377eb8','#4daf4a'])
+
+
+  svg1.append("g")
+    .selectAll("g")
+    // Enter in data = loop group per group
+    .data(data)
+    .join("g")
+      .attr("transform", d => `translate(${x(d.group)}, 0)`)
+    .selectAll("rect")
+    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .join("rect")
+      .attr("x", d => xSubgroup(d.key))
+      .attr("y", d => y(d.value))
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", d => color(d.key));
+
+})
+
   
 
 // second visualization
@@ -27,3 +74,51 @@ let svg2 = d3.select('#vis2')
   .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+
+
+d3.csv("data/Priors_Clean.csv").then (function (data) {
+    const subgroups = data.columns.slice(1)
+
+    const years = data.map(d=> d.year)
+
+    console.log(years)
+
+    const x = d3.scaleBand().domain(years).range([0, width]).padding([.2])
+    svg.append("g").attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x).tickSize(0));
+
+
+    const y = d3.scaleLinear()
+    .domain([0, 40])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+
+    const xSubgroup = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05])
+
+
+    const color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#e41a1c','#377eb8','#4daf4a'])
+
+
+  svg.append("g")
+    .selectAll("g")
+    // Enter in data = loop group per group
+    .data(data)
+    .join("g")
+      .attr("transform", d => `translate(${x(d.group)}, 0)`)
+    .selectAll("circle")
+    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .join("circle")
+      .attr("x", d => xSubgroup(d.key))
+      .attr("y", d => y(d.value))
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", d => color(d.key));
+
+})
